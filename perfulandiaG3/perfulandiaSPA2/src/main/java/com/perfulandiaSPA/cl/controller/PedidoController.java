27c8f961e82e37ebc57a212fcab60338bfac085b
Martin-C-L <Mart.carrascol@duocuc.cl>
2025-06-30 13:55:39 -0400

    test
    testeo para la prueba

--- perfulandiaG3/perfulandiaSPA2/src/main/java/com/perfulandiaSPA/cl/controller/PedidoController.java
+++ perfulandiaG3/perfulandiaSPA2/src/main/java/com/perfulandiaSPA/cl/controller/PedidoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping("/api/v3/pedidos")
+@RequestMapping("/api/v3/pedido")
 public class PedidoController {
     @Autowired
     private PedidoService pedidoService;
