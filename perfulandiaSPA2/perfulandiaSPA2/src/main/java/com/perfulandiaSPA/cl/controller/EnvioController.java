7bbeb80d4b808bdf718d75370ceccf97a9d32aca
Martin-C-L <Mart.carrascol@duocuc.cl>
2025-07-08 19:00:25 -0400

    Update EnvioController.java

--- perfulandiaSPA2/perfulandiaSPA2/src/main/java/com/perfulandiaSPA/cl/controller/EnvioController.java
+++ perfulandiaSPA2/perfulandiaSPA2/src/main/java/com/perfulandiaSPA/cl/controller/EnvioController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v4/envios")
+@RequestMapping("/api/v4/enviosssss")
 public class EnvioController {
 
     @Autowired
